f987761db7206fe9b1b78f9ff867673b578033d0
Oanh Phạm <opham414@gmail.com>
2025-06-08 23:30:21 +0700

    update gg analyst

--- js/config.js
+++ js/config.js
@@ -2,13 +2,5 @@
 export const CONFIG = {
     // Google Analytics Configuration
     GA_TRACKING_ID: 'G-GRVQQ33EXS',
-    
-    // Currency Configuration
-    CURRENCY: 'VND',
-    
-    // Brand Configuration
-    BRAND: 'JUNO',
-    
-    // Minimum order amount for free shipping (in VND)
-    FREE_SHIPPING_THRESHOLD: 300000
+
 }; 
